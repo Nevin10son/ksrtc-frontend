fbffc979e57326c29a019f7aef5f3f5b0be41302
Nevin10son <nevintenson235@gmail.com>
2024-06-13 20:25:03 +0530

    desgining complete

---
+++ src/components/Login.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="https://live.staticflickr.com/65535/50988218236_29b750e044_b.jpg\" class="d-block w-100" alt="..." height={"450px"}/>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="https://keralakaumudi.com/web-news/en/2022/04/NMAN0324725/image/swift.1.1573350.jpg" class="d-block w-100" alt="..."height={"450px"}/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.aanavandi.com/blog/wp-content/uploads/2019/03/prr.jpg" class="d-block w-100" alt="..."height={"450px"}/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+        <h1>User Login</h1>
+        <br />
+      <div className="container">
+      <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+ 
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">User name:</label>
+                        <input type="text" className="form-control" placeholder='Enter the username here'/>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Password:</label>
+                        <input type="text" className="form-control" placeholder='Enter the password here' />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                    <b>New User?<a href=""> Signup</a></b>
+                   
+                </div>
+                <br />
+            </div>
+            
+
+          
+        </div>
+        <br /><br />
+        <div className="footer">
+                
+            </div>
+      </div>
+    
+  )
+}
+
+export default Login
