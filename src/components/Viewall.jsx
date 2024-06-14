056dcb62d90fe26d9da0b5d14d5843f9d0a69c2b
Nevin10son <nevintenson235@gmail.com>
2024-06-14 06:37:55 +0530

    viewall complete

---
+++ src/components/Viewall.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Viewall = () => {
+  return (
+    <div>
+      <h1>View Bus informations</h1>
+      <br />
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Bus Name</th>
+      <th scope="col">Route</th>
+      <th scope="col">Bus no</th>
+      <th scope="col">Driver Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    
+  </tbody>
+</table>
+<br /><br />
+<div className="footer">
+                
+            </div>
+    </div>
+  )
+}
+
+export default Viewall
